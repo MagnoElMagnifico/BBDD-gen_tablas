60494a0217ebcad7712c8cdf495452abe5afd0c0
MarcosG <marcosgranja317@gmail.com>
2023-12-12 23:38:26 +0100

    Actualizar las consultas de reunion

--- consultas-reunion.sql
+++ consultas-reunion.sql
@@ -4,7 +4,7 @@ from libros l, prestamos p
 where l.cod_libro = p.libro
   and (p.fecha_prestamo > '2021-12-31' and p.fecha_prestamo < '2023-01-01')
 
--- Otra alternativa: usando join
+-- Alternativa: usando joins
 select l.titulo
 from libros l join prestamos p on l.cod_libro = p.libro
 where p.fecha_prestamo > '2021-12-31'
@@ -12,6 +12,7 @@ where p.fecha_prestamo > '2021-12-31'
 
 
 -- 2: Obtener el nombre del usuario que más libros ha tomado prestados
+-- TODO: Actualizar esto
 select numlibros.nombre
 from (select s.nombre , count(socio) as num
       from prestamos p, socios s
@@ -22,8 +23,21 @@ where numlibros.num in (select max(num)
                               from prestamos p
                               group by socio) as numlibros)
 
+-- Otra solución más sencilla
+select s.nombre, count (s.cod_socio) as num
+from socios s, prestamos p
+where s.cod_socio = p.socio
+group by s.cod_socio
+having count(s.cod_socio) = (select count(s2.cod_socio) as num
+                             from socios s2, prestamos p2
+                             where s2.cod_socio = p2.socio
+                             group by s2.cod_socio
+                             order by num desc
+                             limit 1)
+
 
 -- 3: Obtener el autor con más libros escritos
+-- TODO: autores con mismo numero de libros escritos
 select a.nombre, a.apellido1, a.apellido2, count(l.cod_libro) libros_escritos
 from autores a
     join escribir e on (a.nombre = e.autor_nombre and a.apellido1 = e.autor_apellido1 and a.apellido2 = e.autor_apellido2)
@@ -33,13 +47,16 @@ order by count(l.cod_libro) desc
 limit 1
 
 
--- 4: Obtener el libro o libros con una valoración mayor a 4.5
-select libro,titulo, avg("valoracion") valoracion_media, count("valoracion") num_valoraciones
-from libros join valoraciones on libro = cod_libro
-group by libro, titulo
-having avg("valoracion") > 4.5
-order by valoracion_media desc
+-- 4: Obtener la valoración media de cada autor según los libros que ha escrito
+select a.nombre, a.apellido1, a.apellido2, avg(v."valoracion") valoracion_autor, count(v."valoracion") num_valoraciones
+from autores a
+     join escribir e on (a.nombre = e.autor_nombre and a.apellido1 = e.autor_apellido1 and a.apellido2 = e.autor_apellido2)
+     join libros l on (e.libro = l.cod_libro)
+     join valoraciones v on (l.cod_libro = v.libro)
+group by a.nombre, a.apellido1, a.apellido2 
+order by valoracion_autor desc
 
+-- TODO: alternativa usando la vista
 
 -- 5: Obtener el nombre de los autores que tienen 5 libros o más en la "ETSE"
 select a.nombre, a.apellido1, a.apellido2
@@ -55,8 +72,11 @@ having count(a.nombre) >= 5
 -- Alternativa: usando joins
 select a.nombre, a.apellido1, a.apellido2
 from autores a
-     join escribir e on (a.nombre = e.autor_nombre and a.apellido1 = e.autor_apellido1 and a.apellido2 = e.autor_apellido2)
-     join libros l on e.libro = l.cod_libro
+    join escribir e on (
+        a.nombre = e.autor_nombre
+        and a.apellido1 = e.autor_apellido1
+        and a.apellido2 = e.autor_apellido2)
+    join libros l on e.libro = l.cod_libro
 where l.biblioteca = 'ETSE'
 group by a.nombre, a.apellido1, a.apellido2
 having count(a.nombre) >= 5
@@ -69,7 +89,7 @@ where p.libro = l.cod_libro
   and p.socio = s.cod_socio
   and s.fecha_egresado = (select min(s2.fecha_egresado) from socios s2)
 
--- Joins
+-- Alternativa: usando joins
 select l.*
 from libros l
     join prestamos p on p.libro = l.cod_libro
@@ -78,10 +98,19 @@ where s.fecha_egresado = (select min(s2.fecha_egresado) from socios s2)
 
 
 -- 7: Obtener el usuario que más libros ha valorado.
-select s.nombre, s.cod_socio  , count(*)
+select s.nombre, s.cod_socio, count(*)
 from socios s , valoraciones v
 where s.cod_socio = v.socio
 group by s.nombre, s.cod_socio 
 having count(*) >= all(select count(*)
-					   from valoraciones v2
-					   group by v2.socio)
+                       from valoraciones v2
+                       group by v2.socio)
+
+-- 8: Los 30 mejores valorados libros disponibles de matemáticas
+select l.*, v.valoracion
+from libros l join valoraciones v on l.cod_libro = v.libro 
+where l.cod_libro in (select ld.cod_libro from libros_disponibles ld)
+order by v.valoracion desc
+limit 30
+
+-- TODO?: mostrar ejemplo sin la vista
