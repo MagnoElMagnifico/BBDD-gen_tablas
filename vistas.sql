8d5536ff70096ad88b90cb790df350102dba5acf
MarcosG <marcosgranja317@gmail.com>
2023-12-12 18:59:23 +0100

    Formatear las vistas mejor

--- vistas.sql
+++ vistas.sql
@@ -1,18 +1,28 @@
 -- 1: Prestamos pendientes
-create view prestamos_pendientes as
-select cod_socio, nombre, apellido1, apellido2, email,
-       cod_libro, titulo, biblioteca,
-       now() - fecha_prestamo as tiempo_restante
+create view prestamos_pendientes (
+    cod_socio, nombre, apellido1, apellido2, email,
+    cod_libro, titulo, biblioteca,
+    tiempo_restante
+)
+as select 
+     cod_socio, nombre, apellido1, apellido2, email,
+     cod_libro, titulo, biblioteca,
+     now() - fecha_prestamo as tiempo_restante
 from socios
      join prestamos on socio = cod_socio
      join libros on libro = cod_libro
 where fecha_devolucion is null 
 
 -- 2: Vista de devoluciones tardías
-create view devoluciones_tarde (cod_libro, titulo, cod_socio, nombre, apellido1, apellido2, email, telefono, tiempo_retrasado) as
-select cod_libro, titulo,
-       cod_socio, nombre, apellido1, apellido2, email, tu.telefono,
-       now() - fecha_prestamo - interval '30 days' as tiempo_retrasado
+create view devoluciones_tarde (
+    cod_libro, titulo,
+    cod_socio, nombre, apellido1, apellido2, email, telefono,
+    tiempo_retrasado
+)
+as select
+     cod_libro, titulo,
+     cod_socio, nombre, apellido1, apellido2, email, tu.telefono,
+     now() - fecha_prestamo - interval '30 days' as tiempo_retrasado
 from socios s
      join prestamos p on p.socio = s.cod_socio
      join libros l on p.libro = l.cod_libro
@@ -31,13 +41,13 @@ where biblioteca = 'Matemáticas'
 except
 select cod_libro, titulo
 from libros l2
-	join bibliotecas b2 on l2.biblioteca = b2.nombre
-	join prestamos p on l2.cod_libro = p.libro
+     join bibliotecas b2 on l2.biblioteca = b2.nombre
+     join prestamos p on l2.cod_libro = p.libro
 where fecha_devolucion is null
 
 -- 4: Valoración media de cada libro, junto con su número de reseñas
 create view valoracion_media (cod_libro, titulo, valoracion_media, num_valoraciones) as
-select libro, titulo, avg("valoracion") valoracion_media, count("valoracion") num_valoraciones
+select libro, titulo, avg("valoracion"), count("valoracion")
 from libros join valoraciones on libro = cod_libro
 group by libro, titulo
-order by valoracion_media desc
+order by avg("valoracion") desc
