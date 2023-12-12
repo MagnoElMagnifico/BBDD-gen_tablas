3d18a0f9a111c29f9a44ce5d1b0af3beb8f4eaee
MarcosG <marcosgranja317@gmail.com>
2023-12-12 16:12:10 +0100

    Completar consultas basicas

--- consultas-basicas.sql
+++ consultas-basicas.sql
@@ -20,3 +20,28 @@ where sueldo < (select avg(sueldo) from bibliotecarios)
 select *
 from autores
 where pais = 'España'
+
+
+-- Obtener el libro con la fecha de publicación más reciente.
+select *
+from libros
+where fecha_pub = (select max(fecha_pub) from libros)
+
+
+-- Obtener todos los datos del bibliotecario que entra más pronto a trabajar.
+select *
+from bibliotecarios
+where hora_entrada = (select min(hora_entrada) from bibliotecarios)
+
+
+-- Obtener todos los datos de los usuarios que no tienen como correo de contacto uno con el dominio “@rai.usc.es”, “@usc.es”.
+select *
+from socios
+where email not like '%@rai.usc.es%'
+  and email not like '%@usc.es%'
+
+
+-- Obtener el nombre de todos los usuarios con grado “Ingeniería Informática”
+select *
+from socios
+where grado = 'Informática'
