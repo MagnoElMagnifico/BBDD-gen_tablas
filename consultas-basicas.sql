e21999e4aaf3cae87e6f128d4627aa74e3534afe
MarcosG <marcosgranja317@gmail.com>
2023-12-17 01:07:28 +0100

    Añadir MER

--- consultas-basicas.sql
+++ consultas-basicas.sql
@@ -25,19 +25,22 @@ where grado = 'Informática'
 -- 5: Obtener los datos de todos bibliotecarios que tengan un sueldo inferior a la media.
 select *
 from bibliotecarios
-where sueldo < (select avg(sueldo) from bibliotecarios)
+where sueldo < (select avg(sueldo)
+                from bibliotecarios)
 
 
 -- 6: Obtener el libro con la fecha de publicación más reciente.
 select *
 from libros
-where fecha_pub = (select max(fecha_pub) from libros)
+where fecha_pub = (select max(fecha_pub)
+                   from libros)
 
 
 -- 7: Obtener todos los datos del bibliotecario que entra más pronto a trabajar.
 select *
 from bibliotecarios
-where hora_entrada = (select min(hora_entrada) from bibliotecarios)
+where hora_entrada = (select min(hora_entrada)
+                      from bibliotecarios)
 
 
 -- 8: Obtener todos los datos de los usuarios que no usen correos de la USC.
