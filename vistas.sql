0e832c5baa563b408f2dc04280b0effb341147de
MarcosG <marcosgranja317@gmail.com>
2023-12-20 15:06:47 +0100

    Añadir ; para poder ejecutar vistas.sql todo seguido

--- vistas.sql
+++ vistas.sql
@@ -9,11 +9,11 @@ create view prestamos_pendientes (
 as select 
      cod_socio, nombre, apellido1, apellido2, email,
      cod_libro, titulo, biblioteca,
-     now() - fecha_prestamo as tiempo_restante
+     now() - fecha_prestamo as tiempo_prestado
 from socios
      join prestamos on socio = cod_socio
      join libros on libro = cod_libro
-where fecha_devolucion is null 
+where fecha_devolucion is null;
 
 
 -- 2: Vista de devoluciones tardías
@@ -25,7 +25,7 @@ create view devoluciones_tarde (
     tiempo_retrasado
 )
 as select
-     cod_libro, titulo,
+     s.cod_libro, s.titulo,
      cod_socio, nombre, apellido1, apellido2, email, tu.telefono,
      now() - fecha_prestamo - interval '30 days' as tiempo_retrasado
 from socios s
@@ -35,7 +35,7 @@ from socios s
            from telefonos_socios join socios on cod_socio = socio
            group by cod_socio) tu using (cod_socio)
 where now() - fecha_prestamo > interval '30 days'
-  and fecha_devolucion is null
+  and fecha_devolucion is null;
 
 
 -- 3: Libros disponibles en la biblioteca de Matemáticas
@@ -43,11 +43,11 @@ create view libros_disponibles (cod_libro, titulo) as
 select distinct l.cod_libro, l.titulo 
 from libros l join prestamos p on l.cod_libro = p.libro
 where p.fecha_devolucion is not null
-  and biblioteca = 'Matemáticas'
+  and biblioteca = 'Matemáticas';
 
 
 -- 4: Valoración media de cada libro, junto con su número de reseñas
 create view valoracion_media (cod_libro, titulo, valoracion_media, num_valoraciones) as
 select libro, titulo, avg("valoracion"), count("valoracion")
 from libros join valoraciones on libro = cod_libro
-group by libro, titulo
+group by libro, titulo;
